6f359c6bdda9b57e8ff7b67ea0577478be56ca41
cda07saba <jeremy.sabadie@2isa.org>
2023-01-18 09:16:42 +0100

    Ajoutez des fichiers projet.

---
+++ TP_Compte/Compte.cs
@@ -0,0 +1,29 @@
+﻿namespace TP_Compte
+{
+    internal class Compte
+    {
+        //Atttributs de la classe:
+        private int Sold = 0;
+        private int soldCode;
+        //Constructeur:
+        public Compte(int sold, int soldCode)
+        {
+            Sold = sold;
+            this.soldCode = soldCode;
+            soldCode += 1;
+        }
+
+
+        //Accesseurs en lecture seule des pro^riétés de la classe:
+        public int Sold1 { get => Sold; private set => Sold = value; }
+        public int SoldCode { get => soldCode; private set => soldCode = value; }
+        //Méthode ToString qui affiche qui permet de consulter le compte:
+        public void ToString()
+        {
+            Console.WriteLine($"Code du compte:{}");
+        }
+
+
+
+    }
+}
